941c4b832844d5fe9841bdfba22f922b3c951e7d
deliamariat <161427654+deliamariat@users.noreply.github.com>
2025-03-12 19:18:42 +0200

    Add files via upload

---
+++ Totolici_Delia_ActivitateSD2025/seminar 3.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+
+typedef struct StructuraMasina Masina;
+
+void afisareMasina(Masina masina) {
+	printf("Id: %d\n", masina.id);
+	printf("Nr usi: %d\n", masina.nrUsi);
+	printf("Pret: %5.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n", masina.serie);
+}
+
+void afisareVectorMasini(Masina* masini, int nrMasini) {
+	for (int i = 0; i < nrMasini; i++) {
+		afisareMasina(masini[i]);
+	}
+}
+
+void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
+	Masina* aux = (Masina*)malloc(sizeof(Masina) * ((*nrMasini) + 1));
+	for (int i = 0; i < (*nrMasini); i++) {
+		aux[i] = (*masini)[i];
+	}
+	aux[(*nrMasini)] = masinaNoua;
+	free(*masini);
+	(*masini) = aux;
+	(*nrMasini)++;
+}
+
+Masina citireMasinaFisier(FILE* file) {
+	Masina m;
+	char buffer[100];
+	char delim[4] = ",;\n";
+	fgets(buffer, 100, file);
+	char* aux = strtok(buffer, delim);
+	m.id = atoi(aux);
+	m.nrUsi = atoi(strtok(NULL, delim));
+	m.pret = atof(strtok(NULL, delim));
+	aux = strtok(NULL, delim);
+	m.model = malloc(sizeof(char) * (strlen(aux) + 1));
+	strcpy_s(m.model, strlen(aux) + 1, aux);
+	aux = strtok(NULL, delim);
+	m.numeSofer = malloc(sizeof(char) * (strlen(aux) + 1));
+	strcpy_s(m.numeSofer, strlen(aux) + 1, aux);
+	aux = strtok(NULL, delim);
+	m.serie = aux[0];
+	return m;
+}
+
+Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
+	FILE* f = fopen(numeFisier, "r");
+	Masina* masina = NULL;
+	while (!feof(f)) {
+		Masina m = citireMasinaFisier(f);
+		adaugaMasinaInVector(&masina, nrMasiniCitite, m);
+	}
+	fclose(f);
+	return masina;
+}
+
+void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
+	for (int i = 0; i < (*nrMasini); i++) {
+		free((*vector)[i].model);
+		free((*vector)[i].numeSofer);
+	}
+	free(*vector);
+	(*vector) = NULL;
+	(*nrMasini) = 0;
+}
+
+int main() {
+	Masina* masini = NULL;
+	int nrMasini = 0;
+	masini = citireVectorMasiniFisier("masini.txt", &nrMasini);
+	afisareVectorMasini(masini, nrMasini);
+	dezalocareVectorMasini(&masini, &nrMasini);
+
+	return 0;
+}
